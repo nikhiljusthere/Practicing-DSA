5859b479ab976c606e8dfb2cab95c2a73962f8f6
nikhiljusthere <nikhil.shrivastava003@gmail.com>
2024-05-23 20:46:13 +0530

    .

---
+++ Pattern/Printing_successive_star_pattern.java
@@ -0,0 +1,23 @@
+package Pattern;
+//Code to print successive star pattern taking input from user
+
+import java.util.Scanner;
+
+public class Printing_successive_star_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int numrows = sc.nextInt();
+        printstarpattern(numrows);
+        sc.close();
+    }
+
+    public static void printstarpattern(int rows){
+        for (int i = 1; i <= rows; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            } 
+            System.out.println();
+        }
+    }
+}
