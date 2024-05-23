f26b0c12187d906019b74407de688b649b19e28c
nikhiljusthere <nikhil.shrivastava003@gmail.com>
2024-05-23 20:46:49 +0530

    .

---
+++ OOPS/Calling_methods_from_different_classes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class calculator{
+    public int add(int n1,int n2){
+        int r = n1 + n2;
+        return r;
+    }
+}
+
+public class Calling_methods_from_different_classes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 20;
+        int num2 = 15;
+        calculator calc = new calculator();
+        int result = calc.add(20,15);
+        System.out.println(result);
+    }
+}
