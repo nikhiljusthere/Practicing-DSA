d2433f4df25a8b4ad51b53c34d534892d683c833
nikhiljusthere <nikhil.shrivastava003@gmail.com>
2024-05-23 19:58:39 +0530

    Added question 5

--- Arrays/leetcode#1929_Concatenation of Array.java
+++ Arrays/leetcode#1929_Concatenation of Array.java
@@ -4,12 +4,12 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n * 2];
+        int[] ans = new int[n * 2]; //creating array of size twice that of nums
 
         for (int i = 0; i < n; i++ )
         {
-            ans[i] = nums[i];
-            ans[i+n] = nums[i];
+            ans[i] = nums[i];   //assigns (i)th element of nums to (i)th element of ans 
+            ans[i+n] = nums[i]; //assigns (i)th element of nums to (i+n)th element of ans
         }
         return ans;
     }
