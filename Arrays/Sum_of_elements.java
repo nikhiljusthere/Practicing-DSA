d2b54f4d85ffc06474bd09246fd4a536a1578ddc
nikhiljusthere <nikhil.shrivastava003@gmail.com>
2024-05-29 21:26:53 +0530

    .

---
+++ Arrays/Sum_of_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sum_of_elements {
+    public static void main(String[] args) {
+        int arr[];
+        System.out.print("Enter size of the array: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            System.out.printf("Enter the element at arr[%d]: ",i);
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + arr[i];
+        }
+
+        System.out.printf("sum of all elements is: %d ",sum);
+
+
+    }
+    
+}
